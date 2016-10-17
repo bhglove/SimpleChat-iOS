78f0267826044f78146bc52466fe2a30e1e6cff8
Benjmain Glover <bhglove@clemson.edu>
2016-10-17 10:32:15 -0400

    init commit

--- bhglove.P4/ConversationController.swift
+++ bhglove.P4/ConversationController.swift
@@ -1,26 +1,26 @@
 //
-//  ViewController.swift
+//  ConversationController.swift
 //  bhglove.P4
 //
-//  Created by Benjamin Glover on 10/14/16.
+//  Created by Benjamin Glover on 10/15/16.
 //  Copyright © 2016 Benjamin Glover. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class ConversationController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
- 
